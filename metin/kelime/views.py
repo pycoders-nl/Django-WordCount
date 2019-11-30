d20f6bd26346299c3c6723161871e09c050bade2
naim ucar <naimens.ucares@gmail.com>
2019-11-30 12:07:31 +0100

    Django-Word

--- metin/kelime/views.py
+++ metin/kelime/views.py
@@ -7,34 +7,47 @@ def index(request):
     return render(request,"index.html")
     
 def sonuc(request):
+
     kelime=Kelime.objects.all()
     if request.method =="POST":
-                        
+       
         text=request.POST['text']
-        kaydet=Kelime(metin_text=text)
-        kaydet.save()
-        text=text.split()
-        kelime_sayisi=str(len(text))
-        maxi={}
-        ozgun=[]
-        for i in text:
-            ozgun+=[i]
+        if text=="":
+            
+            context={'kelime_sayisi':"BOS",
+                'tekrar_kelime':"BOS",
+                'tekrar_kelime_sayisi':"BOS",
+                'ozgun_sayi':"BOS",
+                }
+        
+            return render(request,"sonuc.html",context)
 
-            maxi[text.count(i)]=i
-        tekrar_kelime_sayisi=max(maxi.keys())
-        tekrar_kelime=maxi[tekrar_kelime_sayisi]
-        tekrar_kelime=str(tekrar_kelime)
-        print(len(ozgun))
-        ozgun=list(set(ozgun))
-        ozgun_sayi=len(ozgun)
-        print(ozgun_sayi)
-        context={'kelime_sayisi':kelime_sayisi,
+            
+        else:
+            kaydet=Kelime(metin_text=text)
+            kaydet.save()
+            text=text.split()
+            kelime_sayisi=str(len(text))
+            maxi={}
+            ozgun=[]
+            for i in text:
+                ozgun+=[i]
+
+                maxi[text.count(i)]=i
+            tekrar_kelime_sayisi=max(maxi.keys())
+            tekrar_kelime=maxi[tekrar_kelime_sayisi]
+            tekrar_kelime=str(tekrar_kelime)
+            
+            ozgun=list(set(ozgun))
+            ozgun_sayi=len(ozgun)
+            ozgun_sayi=str(ozgun_sayi)
+
+            context={'kelime_sayisi':kelime_sayisi,
                 'tekrar_kelime':tekrar_kelime,
                 'tekrar_kelime_sayisi':tekrar_kelime_sayisi,
                 'ozgun_sayi':ozgun_sayi,
                 }
         
 
-        print(kelime_sayisi,tekrar_kelime,tekrar_kelime_sayisi)
-        
-    return render(request,"sonuc.html",context)
+    
+            return render(request,"sonuc.html",context)
